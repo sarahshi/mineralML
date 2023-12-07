833bad24f5eca1d179e7d416b27b326232da896f
Sarah Shi <sarah.c.shi@gmail.com>
2023-12-07 00:27:23 -0500

    Remove imblearn fully

--- src/mineralML/core.py
+++ src/mineralML/core.py
@@ -6,8 +6,6 @@ import random
 import numpy as np
 import pandas as pd
 
-from imblearn.over_sampling import RandomOverSampler
-
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
